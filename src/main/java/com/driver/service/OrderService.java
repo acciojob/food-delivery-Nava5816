1e04d77211174e2b8cdac572b39d2f62584d5efe
Navnath Dethe <118654714+Nava5816@users.noreply.github.com>
2023-02-25 20:03:31 +0530

    Update OrderService.java

--- src/main/java/com/driver/service/OrderService.java
+++ src/main/java/com/driver/service/OrderService.java
@@ -2,16 +2,16 @@ package com.driver.service;
 
 import java.util.List;
 
-import com.driver.shared.dto.OrderDto;
+import com.driver.shared.dto.FoodDto;
 
 /**
  * Handle exception cases for all methods which throw Exception
  */
-public interface OrderService {
+public interface FoodService {
 
-	OrderDto createOrder(OrderDto order);
-	OrderDto getOrderById(String orderId) throws Exception;
-	OrderDto updateOrderDetails(String orderId, OrderDto order) throws Exception;
-	void deleteOrder(String orderId) throws Exception;
-	List<OrderDto> getOrders();
+	FoodDto createFood(FoodDto food);
+	FoodDto getFoodById(String foodId) throws Exception;
+	FoodDto updateFoodDetails(String foodId, FoodDto foodDetails) throws Exception;
+	void deleteFoodItem(String id) throws Exception;
+	List<FoodDto> getFoods();
 }
